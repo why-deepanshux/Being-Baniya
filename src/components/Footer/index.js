f60ac9c6232f8c7882573da5186e6685435277e2
why-deepanshux <deepanshusharma12800@gmail.com>
2024-12-31 19:28:02 +0530

    Links updated for socials

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,42 +8,30 @@ const Footer = () => {
     <div class="footer">
       <div class="footer-icons">
         <a
-          href="https://github.com"
+          href="https://github.com/why-deepanshux/Being-Baniya"
           target="_blank"
           aria-label="GitHub"
           class="footer-icon"
         >
-          <img
-            src={Github}
-            alt="GitHub"
-            class="icon"
-          />
+          <img src={Github} alt="GitHub" class="icon" />
           {/* <p>GitHub</p> */}
         </a>
         <a
-          href="https://linkedin.com"
+          href="https://www.linkedin.com/in/deepanshu-sharma-449b661ba/"
           target="_blank"
           aria-label="LinkedIn"
           class="footer-icon"
         >
-          <img
-            src={LinkedIn}
-            alt="LinkedIn"
-            class="icon"
-          />
+          <img src={LinkedIn} alt="LinkedIn" class="icon" />
           {/* <p>LinkedIn</p> */}
         </a>
         <a
-          href="https://twitter.com"
+          href="https://x.com/why_deepanshu"
           target="_blank"
           aria-label="Twitter"
           class="footer-icon"
         >
-          <img
-            src={Twitter}
-            alt="Twitter"
-            class="icon"
-          />
+          <img src={Twitter} alt="Twitter" class="icon" />
           {/* <p>Twitter</p> */}
         </a>
       </div>
